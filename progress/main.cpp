b3fbd53090df24169d58bfd1ac6d3ddeb5c46e7e
Ryan Kitson <ryanak1@gmail.com>
2026-01-28 00:13:21 -0500

    Updated CMakeLists.txt formatting

--- progress/main.cpp
+++ progress/main.cpp
@@ -3,23 +3,23 @@
 #include "label.h"
 
 int main() {
-    dd::DevelDog devel_term(false);
+	dd::DevelDog devel_term(true);
 
-    dd::Style box_style;
-    box_style.fg = dd::RGB{221, 134, 58}; // Halloween Orange
+	dd::Style box_style;
+	box_style.fg = dd::RGB{221, 134, 58}; // Halloween Orange
 
-    dd::Style text_style;
-    text_style.fg = dd::ColorName::White;
-    text_style.attributes = (uint8_t)dd::Attribute::Bold;
+	dd::Style text_style;
+	text_style.fg = dd::ColorName::White;
+	text_style.attributes = (uint8_t)dd::Attribute::Bold;
 
-    // Attach a large box
-    devel_term.attach_component(std::make_unique<dd::Box>(0, 0, 40, 10, box_style));
+	// Attach a large box
+	devel_term.attach_component(std::make_unique<dd::Box>(0, 0, 40, 10, box_style));
 
-    // Attach a label within the box
-    devel_term.attach_component(
-        std::make_unique<dd::Label>(8, 4, "SYSTEM STATUS: ACTIVE", text_style));
+	// Attach a label within the box
+	devel_term.attach_component(
+		std::make_unique<dd::Label>(8, 4, "SYSTEM STATUS: ACTIVE", text_style));
 
-    devel_term.start();
+	devel_term.start();
 
-    return 0;
+	return 0;
 }
